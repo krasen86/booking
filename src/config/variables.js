4de1ca7bfb2f4609a88418bca242b0df00d2a3e4
Krasen Parvanov <krasenparvanov@gmail.com>
2020-11-30 15:44:19 +0100

    Update logic for topic subscription for booking component
    Co-Authored-By: Samuel Gunnarsson <samuelg@student.chalmers.se>

--- src/config/variables.js
+++ src/config/variables.js
@@ -1,6 +1,6 @@
 module.exports = {
     URL: 'mqtt://127.0.0.1', // localhost
-    REQUEST_TOPIC: 'request/#',
+    REQUEST_TOPIC: 'request',
     RESPONSE_TOPIC: 'response',
     TIMESLOT_TOPIC: 'timeslot',
     CONFIRMATION_TOPIC: 'confirmation'
