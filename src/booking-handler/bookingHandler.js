476aee501d141c4822f3f4ed3775c0ffbf045e86
Krasen Parvanov <krasenparvanov@gmail.com>
2020-11-30 23:14:20 +0100

    #1 Synchronise the data interface format of the sending booking and start using async js function for data loading from file

--- src/booking-handler/bookingHandler.js
+++ src/booking-handler/bookingHandler.js
@@ -8,16 +8,31 @@ class BookingHandler {
     checkConfirmation(confirmation) {
         let publisher = new Publisher();
         let bookingIO = new BookingIO();
-        let bookingRequest = bookingIO.readRequest(confirmation);
-        const index = bookingRequest.findIndex(item => item.time === confirmation.time);
-        let booking = bookingRequest[index]
+        let bookingRequests = bookingIO.readRequest(confirmation);
 
-        if (confirmation.available) {
-            publisher.publishBookingResponse(booking);
-        } else {
-            booking.error = 'Time not available'
-            publisher.publishBookingResponse(booking.error);
-        }
+        bookingRequests.then(requests => {
+            let list = JSON.parse(requests);
+            const index = list.findIndex(item => item.time === confirmation.time);
+            let booking = list[index]
+            if (confirmation.available && booking !== undefined) {
+                let success = {};
+                success.userid = booking.userid;
+                success.requestid = booking.requestid;
+                success.time = booking.time;
+                bookingIO.deleteRequest(confirmation);
+                publisher.publishBookingResponse(success);
+            } else if (!confirmation.available && booking !== undefined) {
+                let error = {};
+                error.userid = booking.userid;
+                error.requestid = booking.requestid;
+                publisher.publishBookingResponse(error);
+            } else {
+                console.log("Error no matching booking requests with availability confirmation")
+            }
+        }).catch(err => {
+            console.log(err)
+        })
+
     }
 
 
