8fe71908b48fce36eb3d2eaaa1395620530fc35f
samuelg <samuelg@student.chalmers.se>
2020-12-02 10:44:30 +0100

    fixing a typo in topic name
    Co-Authored-By: Krasen Anatoliev Parvanov <krasen@student.chalmers.se>

--- src/config/variables.js
+++ src/config/variables.js
@@ -2,7 +2,7 @@ module.exports = {
     URL: 'mqtt://127.0.0.1', // localhost
     REQUEST_TOPIC: 'request',
     RESPONSE_TOPIC: 'response',
-    TIMESLOT_TOPIC: 'timeslot',
+    TIMESLOT_TOPIC: 'timeslots',
     CONFIRMATION_TOPIC: 'confirmation',
     DIRECTORY: './booking-data/',
     DIRECTORY_BOOKING: './booking-data/booking-',
