7e28acd58734e1d4e0d293d40febc76d259aa916
chrtsa <chrtsa@student.chalmers.se>
2020-12-12 12:25:59 +0200

    #3 Add logging functionality to the received booking requests
    Extend rate limiter's functionality by logging all the incoming booking requests based on their level (warning or info) into the respective log file.
    
    Co-Authored-By: Johann Tammen <tammen@student.chalmers.se>

--- src/services/requestRateLimiter.js
+++ src/services/requestRateLimiter.js
@@ -1,26 +1,50 @@
-
+const winston = require('winston');
 const RateLimiter = require('limiter').RateLimiter;
 const {BookingController} = require("../booking-handler/bookingController");
-
+const { createLogger, format} = require('winston');
+const { combine, timestamp, printf } = format;
+const myFormat = printf(({ level, message, label, timestamp }) => {
+    return `${timestamp} [${label}] ${level}: ${message}`;
+});
 
 class RequestRateLimiter {
     constructor(tokensPerInterval, Interval) {
         this.limiter = new RateLimiter(tokensPerInterval, Interval, true);
         this.counter = 0
         this.bookingController = new BookingController();
+        this.logger = createLogger({
+            format: combine(
+                timestamp(),
+                myFormat
+            ),
+            transports: [
+                new winston.transports.File({ filename: './logs/requests.log' })
+            ]
+        });
     }
 
     fire(message) {
-       var _this = this
+        let _this = this
+        let request = JSON.parse(message)
 
         this.limiter.removeTokens(1, function(err, remainingRequests) {
             console.log(_this.counter++)
 
             if (remainingRequests < 0) {
                 console.log('too many request to handle')
+                _this.logger.log({
+                    label: 'Denied',
+                    level: 'warn',
+                    message: 'Request id: ' + request.requestid + ' User id: ' + request.userid + ' Dentist id: ' + request.dentistid
+                });
             } else {
                 console.log('request accepted')
                 _this.bookingController.processRequest(message)
+                _this.logger.log({
+                    label: 'Accepted',
+                    level: 'info',
+                    message: 'Request id: ' + request.requestid + ' User id: ' + request.userid + ' Dentist id: ' + request.dentistid
+                });
             }
         })
 
